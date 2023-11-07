940a62ec1984387aa2b9737bba59c41c5bd30fa9
Aravind Sunil Kumar <Aravindsunilkumar@users.noreply.github.com>
2023-11-07 23:29:44 +0100

    Created Cipher

---
+++ Caeser-Cipher.py
@@ -0,0 +1,7 @@
+# Creating a list of Alphabets
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+#Asking the user either encode or decode the message
+direction = input("Type'E' for Encode and 'D' for Decoding the message: \n")
+#Enter the desired message
+text = input('Type your message:\n').lower()
+shift=int(input(''))
---
+++ Readme.md
@@ -0,0 +1,2 @@
+### Caeser Cipher
+Here we try to make a caser cipher using python programming. A funtion is created then using it to encode and decode a message by shifting the letter by the number the user defines.
