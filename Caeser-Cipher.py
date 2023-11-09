aa8488ca016a612e54ac0e075ea84a020e99ba3d
Aravind Sunil Kumar <Aravindsunilkumar@users.noreply.github.com>
2023-11-09 08:36:52 +0100

    updated code

--- Caeser-Cipher.py
+++ Caeser-Cipher.py
@@ -1,17 +1,29 @@
 # Creating a list of Alphabets
 alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
-#Asking the user either encode or decode the message
-cipher_direction = input("Type 'Encode' for encodeing and 'Decode' for Decoded the message: \n")
-#Enter the desired message
-text = input('Type your message:\n').lower()
-shift=int(input('Type the shift number'))
-def caeser(start_text,shift_amount,cipher_direction):
+
+
+def caesar(start_text,shift_amount,cipher_direction):
     end_text=""
     if cipher_direction == 'decode':
             shift_amount *= -1
-    for letter in start_text:
-        position = alphabet.index(letter)
-        new_positon = position +shift_amount
-        end_text += alphabet[new_positon]
+    for char in start_text:
+        if char in alphabet:
+            position = alphabet.index(char)
+            new_positon = position +shift_amount
+            end_text += alphabet[new_positon]
+        else:
+             end_text += char
     print(f"the {cipher_direction} of the text {end_text}")
-caeser(start_text= text, shift_amount=shift,cipher_direction=cipher_direction)
+from art import logo
+print(logo)
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
